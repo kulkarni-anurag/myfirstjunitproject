7eebf3234950c5b16c785c1388842f8fb3ed9f7a
Anurag Kulkarni <kanurag0801@gmail.com>
2022-04-28 14:24:57 +0530

    Written first unit test case

--- src/test/java/com/anurag/junit/helper/StringHelperTest.java
+++ src/test/java/com/anurag/junit/helper/StringHelperTest.java
@@ -7,6 +7,12 @@ import org.junit.Test;
 public class StringHelperTest {
     @Test
     public void test(){
-        assertEquals("ABC", "ABCD");
+        StringHelper stringHelper = new StringHelper();
+
+        // 1. AACD => CD 2. ACD => CD 3. CDEF => CDEF 4. CDAA => CDAA
+        assertEquals("CD", stringHelper.turncateAInFirst2Positions("AACD"));
+        assertEquals("CD", stringHelper.turncateAInFirst2Positions("ACD"));
+        assertEquals("CDEF", stringHelper.turncateAInFirst2Positions("CDEF"));
+        assertEquals("CDAA", stringHelper.turncateAInFirst2Positions("CDAA"));
     }
 }
