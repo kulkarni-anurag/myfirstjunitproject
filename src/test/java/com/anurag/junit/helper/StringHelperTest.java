88df2001779a4da65d6e98c33c9a0a08b02487ec
Anurag Kulkarni <kanurag0801@gmail.com>
2022-04-28 14:41:21 +0530

    Properly formatted all the tests

--- src/test/java/com/anurag/junit/helper/StringHelperTest.java
+++ src/test/java/com/anurag/junit/helper/StringHelperTest.java
@@ -5,14 +5,30 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class StringHelperTest {
-    @Test
-    public void test(){
-        StringHelper stringHelper = new StringHelper();
 
-        // 1. AACD => CD 2. ACD => CD 3. CDEF => CDEF 4. CDAA => CDAA
+    StringHelper stringHelper = new StringHelper();
+
+    @Test
+    public void testTurncateAInFirst2Positions_AInFirst2Positions(){
+        // 1. AACD => CD
         assertEquals("CD", stringHelper.turncateAInFirst2Positions("AACD"));
+    }
+
+    @Test
+    public void testTurncateAInFirst2Positions_AInFirstPosition(){
+        // 2. ACD => CD
         assertEquals("CD", stringHelper.turncateAInFirst2Positions("ACD"));
+    }
+
+    @Test
+    public void testTurncateAInFirst2Positions_NoAInTheString(){
+        // 3. CDEF => CDEF
         assertEquals("CDEF", stringHelper.turncateAInFirst2Positions("CDEF"));
+    }
+
+    @Test
+    public void testTurncateAInFirst2Positions_AInLast2Positions(){
+        // 4. CDAA => CDAA
         assertEquals("CDAA", stringHelper.turncateAInFirst2Positions("CDAA"));
     }
 }
