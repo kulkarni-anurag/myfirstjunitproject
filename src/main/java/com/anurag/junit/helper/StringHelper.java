4d63c78cfbf4e0118c352423f9286e96272fd681
Anurag Kulkarni <kanurag0801@gmail.com>
2022-04-28 14:10:55 +0530

    First Junit test created

---
+++ src/main/java/com/anurag/junit/helper/StringHelper.java
@@ -0,0 +1,29 @@
+package com.anurag.junit.helper;
+
+public class StringHelper {
+    public String turncateAInFirst2Positions(String str){
+        if(str.length() <= 2){
+            return str.replaceAll("A", "");
+        }
+
+        String first2Chars = str.substring(0, 2);
+        String stringMinusFirst2Chars = str.substring(2);
+
+        return first2Chars.replaceAll("A", "replacement") + stringMinusFirst2Chars;
+    }
+
+    public boolean areFirstAndLastTwoCharactersTheSame(String str){
+        if(str.length() <= 1){
+            return false;
+        }
+        
+        if(str.length() == 2){
+            return true;
+        }
+
+        String first2Chars = str.substring(0, 2);
+        String last2Chars = str.substring(str.length() - 2);
+
+        return first2Chars.equals(last2Chars);
+    }
+}
---
+++ src/test/java/com/anurag/junit/helper/StringHelperTest.java
@@ -0,0 +1,12 @@
+package com.anurag.junit.helper;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class StringHelperTest {
+    @Test
+    public void test(){
+        assertEquals("ABC", "ABCD");
+    }
+}
