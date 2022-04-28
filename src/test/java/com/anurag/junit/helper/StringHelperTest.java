7c543a77846309eba23eaf1f7d5f21f07948b051
Anurag Kulkarni <kanurag0801@gmail.com>
2022-04-28 16:23:53 +0530

    Second Unit test complete

--- src/test/java/com/anurag/junit/helper/StringHelperTest.java
+++ src/test/java/com/anurag/junit/helper/StringHelperTest.java
@@ -1,6 +1,8 @@
 package com.anurag.junit.helper;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -30,5 +32,29 @@ public class StringHelperTest {
     public void testTurncateAInFirst2Positions_AInLast2Positions(){
         // 4. CDAA => CDAA
         assertEquals("CDAA", stringHelper.turncateAInFirst2Positions("CDAA"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_BasicFalseScenario(){
+        // 1. ABCD => false
+        assertFalse(stringHelper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_BasicTrueScenario(){
+        // 2. ABAB => true
+        assertTrue(stringHelper.areFirstAndLastTwoCharactersTheSame("ABAB"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_Only1CharacterFalse(){
+        // 3. A => false
+        assertFalse(stringHelper.areFirstAndLastTwoCharactersTheSame("A"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_Only2CharactersTrue(){
+        // 4. AB => true
+        assertTrue(stringHelper.areFirstAndLastTwoCharactersTheSame("AB"));
     }
 }
